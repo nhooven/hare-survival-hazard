c4695d20dafa756034b0f05b2db54457e21d373d
nhooven <nathan.d.hooven@gmail.com>
2023-04-11 15:02:34 -0700

    Initial commit

---
+++ 01 - Simulate time-to-event data.R
@@ -0,0 +1,19 @@
+# Project: WSU Snowshoe Hare and PCT Project
+# Subproject: Survival and hazard modeling
+# Script: 01 - Simulate time-to-event data and fit model
+# Author: Nathan D. Hooven, Graduate Research Assistant
+# Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
+# Date began: 11 Apr 2023
+# Date completed: 
+# Date last modified: 
+# R version: 4.2.2
+
+#_______________________________________________________________________________________________
+# 1. Load required packages ----
+#_______________________________________________________________________________________________
+
+library(tidyverse)       # manipulate and clean data
+
+#_______________________________________________________________________________________________
+# 2. Simulate time-to-event data ----
+#_______________________________________________________________________________________________
