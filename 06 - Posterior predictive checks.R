c0f3fcfca39cc38f4c07ab893ffb554bd05eeb9b
nhooven <nathan.d.hooven@gmail.com>
2025-11-18 11:24:35 -0800

    Completed simulated KM-curves, and added confidence envelope for empirical curve

--- 06 - Posterior predictive checks.R
+++ 06 - Posterior predictive checks.R
@@ -5,7 +5,7 @@
 # Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
 # Date began: 17 Nov 2025 
 # Date completed: 17 Nov 2025
-# Date last modified: 17 Nov 2025 
+# Date last modified: 18 Nov 2025 
 # R version: 4.2.2
 
 #_______________________________________________________________________________________________
@@ -121,6 +121,49 @@ fates.1.split <- split(fates.1, fates.1$study.week)
 # lapply() was a good thought, but I think just looping by iteration and doing the summation
 # within the function is probably fine. We can also print the status by iteration
 
+# helper function 
+# functions
+calc_intensity <- function (y) {
+  
+  # BLH spline prediction
+  # weights w
+  w <- t(as.matrix(as.numeric(c(iter.draw[paste0("w", ".", y$sex_forest, "..1.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..2.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..3.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..4.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..5.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..6.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..7.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..8.")],
+                                iter.draw[paste0("w", ".", y$sex_forest, "..9.")]))))
+  
+  # multiply and sum to create "normal" scale spline prediction
+  w.by.b.sum <- apply(sweep(basis.pred, 2, w, `*`), 1, sum)
+  
+  # add to intercept and exponentiate
+  blh = as.numeric(exp(iter.draw[paste0("a0.", y$sex_forest, ".")] + w.by.b.sum[y$week]))
+  
+  # coefficient prediction (must be on the log scale!)
+  coef.pred = exp(log(iter.draw$hr_bci) * y$BCI.s +
+                    log(iter.draw$hr_pil_total1) * y$post1 * y$pil +
+                    log(iter.draw$hr_pil_total2) * y$post2 * y$pil +
+                    log(iter.draw$hr_ret_total1) * y$post1 * y$ret +
+                    log(iter.draw$hr_ret_total2) * y$post2 * y$ret)
+  
+  # total intensity
+  intens = blh * coef.pred
+  
+  # Poisson draw
+  pois.draw = rpois(n = 1, intens)
+  
+  # pack into a df
+  y.intens.pois <- data.frame(intens = intens,
+                              pois.draw = ifelse(pois.draw > 0, 1, pois.draw))
+  
+  return(y.intens.pois)
+  
+}
+
 #_______________________________________________________________________________________________
 
 bayes_p_mort <- function(
@@ -129,48 +172,6 @@ bayes_p_mort <- function(
   response = "y.mort.scen1"
   
   ) {
-  
-  # functions
-  calc_intensity <- function (y) {
-    
-    # BLH spline prediction
-    # weights w
-    w <- t(as.matrix(as.numeric(c(iter.draw[paste0("w", ".", y$sex_forest, "..1.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..2.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..3.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..4.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..5.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..6.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..7.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..8.")],
-                                  iter.draw[paste0("w", ".", y$sex_forest, "..9.")]))))
-    
-    # multiply and sum to create "normal" scale spline prediction
-    w.by.b.sum <- apply(sweep(basis.pred, 2, w, `*`), 1, sum)
-    
-    # add to intercept and exponentiate
-    blh = as.numeric(exp(iter.draw[paste0("a0.", y$sex_forest, ".")] + w.by.b.sum[y$week]))
-    
-    # coefficient prediction (must be on the log scale!)
-    coef.pred = exp(log(iter.draw$hr_bci) * y$BCI.s +
-                    log(iter.draw$hr_pil_total1) * y$post1 * y$pil +
-                    log(iter.draw$hr_pil_total2) * y$post2 * y$pil +
-                    log(iter.draw$hr_ret_total1) * y$post1 * y$ret +
-                    log(iter.draw$hr_ret_total2) * y$post2 * y$ret)
-    
-    # total intensity
-    intens = blh * coef.pred
-    
-    # Poisson draw
-    pois.draw = rpois(n = 1, intens)
-    
-    # pack into a df
-    y.intens.pois <- data.frame(intens = intens,
-                                pois.draw = ifelse(pois.draw > 0, 1, pois.draw))
-    
-    return(y.intens.pois)
-    
-  }
   
   # loop through iterations
   discrep.sim.all.draws <- vector(length = nrow(draws))
@@ -201,8 +202,10 @@ bayes_p_mort <- function(
       
       # calculate the conditional discrepancy
       # Dj = ((sim/obs events - predicted events)^2) / predicted events
-      discrep.sim[j] = sum(((sim.events$pois.draw - sim.events$intens)^2) / sim.events$intens)
-      discrep.obs[j] = sum(((focal.week[[response]] - sim.events$intens)^2) / sim.events$intens)
+      discrep.sim[j] = ((sum(sim.events$pois.draw) - sum(sim.events$intens))^2) / sum(sim.events$intens)
+      discrep.obs[j] = ((sum(focal.week[[response]]) - sum(sim.events$intens))^2) / sum(sim.events$intens)
+      
+      
       
     }
     
@@ -250,7 +253,7 @@ bayes.diff <- data.frame(
 # 7. Plot and calculate Bayes p-value ----
 #_______________________________________________________________________________________________
 
-length(which(bayes.diff$diff > 0)) / nrow(bayes.diff) # 0.4
+length(which(bayes.diff$diff > 0)) / nrow(bayes.diff) # 0.12
 
 # distribution
 ggplot(data = bayes.diff,
@@ -286,8 +289,8 @@ ggplot(data = bayes.diff,
   xlab("Observed discrepancy") +
   ylab("Simulated discrepancy") +
   
-  coord_cartesian(xlim = c(5000, 9000),
-                  ylim = c(5000, 9000))
+  coord_cartesian(xlim = c(100, 250),
+                  ylim = c(100, 250))
 
 #_______________________________________________________________________________________________
 # 8. Prepare dataset for Kaplan-Meier curves ----
@@ -332,9 +335,13 @@ kap_mei <- function (
   # loop through all follow-up times
   all.S <- vector(length = max(x$t))
   s.i <- vector(length = max(x$t))
+  all.ci.low <-vector(length = max(x$t))
+  all.ci.upp <-vector(length = max(x$t))
   
   all.S[1] = 1.0 # initialize at 100%
   s.i[1] = 1.0 # initialize at 100%
+  all.ci.low[1] <- 1.0
+  all.ci.upp[1] <- 1.0
   
   for (i in 2:max(x$t)) {
     
@@ -347,11 +354,19 @@ kap_mei <- function (
     # cumulative survival at interval
     all.S[i] = prod(s.i[1:i])
     
+    # calculate 90 % confidence interval
+    focal.var = ((all.S[i]^2) * (1 - all.S[i])) / nrow(indivs.i)
+      
+    all.ci.low[i] = all.S[i] - (1.64 * sqrt(focal.var))
+    all.ci.upp[i] = all.S[i] + (1.64 * sqrt(focal.var))
+    
   }
   
   # df for returning
   km.df <- data.frame(t = 1:max(x$t),
-                      S = all.S)
+                      S = all.S,
+                      ci.low = all.ci.low,
+                      ci.upp = all.ci.upp)
   
   return(km.df)
   
@@ -366,6 +381,10 @@ ggplot(data = km.df,
   
   theme_bw() +
   
+  geom_ribbon(aes(ymin = ci.low,
+                  ymax = ci.upp),
+              alpha = 0.25) +
+  
   geom_line() +
   
   theme(panel.grid = element_blank()) +
@@ -375,3 +394,249 @@ ggplot(data = km.df,
   ylab("Cumulative survival") +
   
   coord_cartesian(ylim = c(0, 1))
+
+#_______________________________________________________________________________________________
+# 9. Simulate lifetimes ----
+
+# Here we'll loop through each iteration (take a subsample first),
+# loop through each deployment (n = 383),
+# applying a calc intensity function on an 86-week dataset,
+# take a Poisson draw and truncate the dataset after the first event
+# then apply the K-M curve function
+
+# the output will be a collection of K-M curves
+
+# new calc_intensity function
+calc_intensity_2 <- function (y) {
+  
+  # BLH spline prediction
+  # weights w
+  w <- t(as.matrix(as.numeric(c(iter.draw[paste0("w", ".", y$sex_forest[1], "..1.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..2.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..3.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..4.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..5.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..6.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..7.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..8.")],
+                                iter.draw[paste0("w", ".", y$sex_forest[1], "..9.")]))))
+  
+  # multiply and sum to create "normal" scale spline prediction
+  w.by.b.sum <- apply(sweep(basis.pred, 2, w, `*`), 1, sum)
+  
+  # add to intercept and exponentiate
+  blh = as.numeric(exp(iter.draw[paste0("a0.", y$sex_forest, ".")] + w.by.b.sum[y$week]))
+  
+  # coefficient prediction (must be on the log scale!)
+  coef.pred = exp(log(iter.draw$hr_bci) * y$BCI.s +
+                    log(iter.draw$hr_pil_total1) * y$post1 * y$pil +
+                    log(iter.draw$hr_pil_total2) * y$post2 * y$pil +
+                    log(iter.draw$hr_ret_total1) * y$post1 * y$ret +
+                    log(iter.draw$hr_ret_total2) * y$post2 * y$ret)
+  
+  # total intensity
+  intens = blh * coef.pred
+  
+  # Poisson draw
+  pois.draw = rpois(n = 1, intens)
+  
+  # pack into a df
+  y.intens.pois <- data.frame(intens = intens,
+                              pois.draw = rpois(length(intens), intens))
+  
+  return(y.intens.pois)
+  
+}
+
+#_______________________________________________________________________________________________
+
+sim_lifetime <- function (
+    
+  x = fates.2,
+  draws = model.fit.1
+  
+) {
+  
+  # loop through draws
+  all.km.curves <- data.frame()
+  
+  for (i in 1:nrow(draws)) {
+    
+    iter.draw = draws[i, ]
+    
+    # apply to each deployment
+    deploy.split <- split(x, x$deployment.1)
+    
+    # function
+    sim_lifetime_byDeploy <- function (z) {
+      
+      # create new data.frame to fill
+      suppressMessages(
+      
+      deploy.df <- data.frame(
+        
+        deployment.1 = z$deployment.1[1],
+        sex_forest = z$sex_forest[1],
+        t = 1:200,
+        study.week = seq(z$study.week[1], z$study.week[1] + 19),
+        post1 = z$post1[1],
+        post2 = z$post2[1],
+        ret = z$ret[1],
+        pil = z$pil[1],
+        BCI.s = z$BCI.s[1]
+        
+      ) %>%
+        
+        mutate(
+          
+          # subtract 160 (max week) from anything above it in study.week to recycle
+          study.week = ifelse(study.week > 160,
+                             (study.week - 160) + 4,    # so the correct week of the year will be attributed
+                              study.week)
+          
+        ) %>%
+        
+        # attribute correct week of the year
+        left_join(
+          
+          day.lookup %>%
+            
+            dplyr::select(study.year.week,
+                          study.week) %>%
+            
+            rename(week = study.year.week) %>%
+            
+            group_by(study.week) %>%
+            
+            slice(1)
+          
+        )
+      
+      )
+      
+      # calculate intensity and bind
+      deploy.df <- cbind(deploy.df, calc_intensity_2(y = deploy.df))
+      
+      # extract lifetime
+      focal.lifetime <- data.frame(
+        
+        iter = i,
+        deployment = z$deployment.1[1],
+        lifetime = ifelse(
+          
+          1 %in% deploy.df$pois.draw,
+          which(deploy.df$pois.draw > 0)[1],
+          200                                 # we'll probably truncate this dataset
+                                              # since the max weeks we monitored a hare was 86
+        )
+        
+      )
+      
+      # return
+      return(focal.lifetime)
+      
+    }
+    
+    # apply function
+    all.iter.lifetimes <- do.call(rbind, lapply(deploy.split, sim_lifetime_byDeploy))
+    
+    # calculate K-M curve
+    # create dataset
+    
+    # blank df
+    km.data <- data.frame()
+      
+    # loop
+    for (j in 1:nrow(all.iter.lifetimes)) {
+      
+      # vector of zero observations + 1
+      zero.vector <- vector(length = all.iter.lifetimes$lifetime[all.iter.lifetimes$deployment == j] - 1)
+      zero.vector[1:length(zero.vector)] <- 0
+      zero.vector[length(zero.vector) + 1] <- 1
+      
+      # pack into df
+      focal.df <- data.frame(
+        
+        j = j,
+        t = 1:length(zero.vector),
+        event = zero.vector
+        
+      )
+      
+      km.data <- rbind(km.data, focal.df)
+      
+    }
+    
+    # truncate data at 100 weeks
+    km.data.1 <- km.data %>% filter(t < 101)
+    
+    # apply KM function
+    km.curve.data <- kap_mei(x = km.data.1,
+                             response = "event")
+    
+    # add in iteration
+    km.curve.data$iter = i
+    
+    # pack into df
+    all.km.curves <- rbind(all.km.curves, km.curve.data)
+    
+    # print status (every 100 iterations)
+    
+    if (i %% 5 == 0) {
+      
+      print(paste0("Completed iteration ", i, " of ", nrow(draws))) 
+      
+    }
+
+  }
+  
+  return(all.km.curves)
+  
+}
+
+#_______________________________________________________________________________________________
+# 9a. Apply function ----
+#_______________________________________________________________________________________________
+
+km.curves <- sim_lifetime(x = fates.2,
+                          draws = model.fit.1[sample(1:15000, size = 50), ])
+
+#_______________________________________________________________________________________________
+# 10. Plot with empirical curve ----
+#_______________________________________________________________________________________________
+
+# plot
+ggplot() +
+  
+  theme_bw() +
+  
+  # simulated curves
+  geom_line(data = km.curves,
+            aes(x = t,
+                y = S,
+                group = iter),
+            color = "aquamarine3",
+            linewidth = 0.25) +
+  
+  # empirical curve confidence envelope
+  geom_ribbon(data = km.df,
+              aes(x = t,
+                  y = S,
+                  ymin = ci.low,
+                  ymax = ci.upp),
+              alpha = 0.15) +
+  
+  # empirical curve
+  geom_line(data = km.df,
+            aes(x = t,
+                y = S)) +
+  
+  theme(panel.grid = element_blank(),
+        axis.text = element_text(color = "black")) +
+  
+  xlab("Weeks since deployment") +
+  
+  ylab("Cumulative survival") +
+  
+  coord_cartesian(ylim = c(0, 1),
+                  xlim = c(4.25, 70))
