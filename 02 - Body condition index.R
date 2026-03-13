e06de097edd70c76f0cd0b45a9e45f8cfd2c682c
nhooven <nathan.d.hooven@gmail.com>
2026-03-13 10:33:23 -0700

    changed from a priori imputation to model-based

--- 02 - Body condition index.R
+++ 02 - Body condition index.R
@@ -5,7 +5,7 @@
 # Email: nathan.hooven@wsu.edu / nathan.d.hooven@gmail.com
 # Date began: 13 Nov 2025
 # Date completed: 13 Nov 2025
-# Date last modified: 03 Feb 2026 
+# Date last modified: 09 Mar 2026 
 # R version: 4.4.3
 
 #_______________________________________________________________________________________________
@@ -157,126 +157,32 @@ fates.cc.summary <- fates.cc %>%
 fates.cc.summary
 
 #_______________________________________________________________________________________________
-# 6. Use each imputation strategy to add BCI values to each missing observation ----
-
-# STRATEGY 1: impute missing mass and HFL independently, then calculate BCI
-# STRATEGY 2: impute mean BCI for any incomplete observation (i.e., missing either variable)
-
-#_______________________________________________________________________________________________
-# 6a. Write function ----
+# 6. Set up deployment-specific dataset for model imputation ----
 #_______________________________________________________________________________________________
 
-impute_bci <- function (x) {
+fates.deploy <- fates.1 %>%
   
-  # deployment has both an HFL and a mass
-  if (is.na(x$HFL[1]) == F & is.na(x$Final.mass[1]) == F) {
-    
-    # S1 and S2 are the same in this case
-    x$BCI.1 = (x$Final.mass * 1000) / (x$HFL * 10)
-    x$BCI.2 = (x$Final.mass * 1000) / (x$HFL * 10)
-    
-  }
+  dplyr::select(deployment.1,
+                Sex.1,
+                HFL,
+                Final.mass) %>%
   
-  # deployment is missing HFL but not mass
-  if (is.na(x$HFL[1]) == T & is.na(x$Final.mass[1]) == F) {
-    
-    # impute missing morphometric
-    x$HFL = fates.cc.summary$hfl.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-    # S1
-    x$BCI.1 = (x$Final.mass * 1000) / (x$HFL * 10)
-    
-    # S2
-    x$BCI.2 = fates.cc.summary$bci.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-  }
-  
-  # deployment is missing mass but not HFL
-  if (is.na(x$HFL[1]) == F & is.na(x$Final.mass[1]) == T) {
-    
-    # impute missing morphometric
-    x$Final.mass = fates.cc.summary$mass.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-    # S1
-    x$BCI.1 = (x$Final.mass * 1000) / (x$HFL * 10)
-    
-    # S2
-    x$BCI.2 = fates.cc.summary$bci.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-  }
-  
-  # deployment is missing both HFL and mass
-  if (is.na(x$HFL[1]) == T & is.na(x$Final.mass[1]) == T) {
-    
-    # S1
-    x$BCI.1 = fates.cc.summary$bci.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-    # S2
-    x$BCI.2 = fates.cc.summary$bci.mean[fates.cc.summary$Sex.1 == x$Sex.1[1]]
-    
-  }
-  
-  return(x)
-  
-}
-
-#_______________________________________________________________________________________________
-# 6b. Apply function ----
-#_______________________________________________________________________________________________
-
-fates.imputed <- do.call(rbind, lapply(split(fates, fates$deployment.1), impute_bci))
-
-#_______________________________________________________________________________________________
-# 6c. Compare distributions for each strategy ----
-#_______________________________________________________________________________________________
-
-# consolidate
-fates.imputed.1 <- fates.imputed %>%
-  
-  group_by(deployment.1) %>%
-  
-  slice(1)
-
-# plot a 1:1 graph
-ggplot(data = fates.imputed.1,
-       aes(x = BCI.1,
-           y = BCI.2)) +
-  
-  theme_bw() +
-  
-  geom_abline(intercept = 0,
-              slope = 1,
-              linetype = "dashed") +
-  
-  geom_point() +
-  
-  coord_cartesian(xlim = c(6, 13),
-                  ylim = c(6, 13))
-
-# sex-specific distributions
-ggplot(data = fates.imputed.1 %>% pivot_longer(cols = c("BCI.1", "BCI.2"))) +
-  
-  theme_bw() +
-  
-  facet_wrap(~ name) +
-  
-  geom_density(aes(x = value,
-                   color = as.factor(Sex.1),
-                   fill = as.factor(Sex.1)),
-               alpha = 0.25) +
-  
-  theme(panel.grid = element_blank(),
-        legend.position = "none") +
-  
-  scale_x_continuous(breaks = c(7, 8, 9, 10, 11, 12)) +
-  
-  xlab("Body condition index")
-
-# unsurprisingly, there is more prob. density at the sex-specific means for BCI.2
-# we can try both in the model and see if these imputed data still have a signal
+  # calculate BCI (implicitly for complete cases)
+  mutate(BCI = (Final.mass * 1000) / (HFL * 10))
 
 #_______________________________________________________________________________________________
 # 7. Write to file ----
 #_______________________________________________________________________________________________
 
-write.csv(fates.imputed, "Cleaned data/fates_final_cleaned_2.csv")
+fates.deploy.list <- list(
+  
+  # deployment df
+  fates.deploy = fates.deploy,
+  
+  # standardization parameters
+  bci.mean = mean(fates.deploy$BCI, na.rm = T),
+  bci.sd = sd(fates.deploy$BCI, na.rm = T)
+  
+)
+
+saveRDS(fates.deploy.list, "Cleaned data/fates_deploy.rds")
